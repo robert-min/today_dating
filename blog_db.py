15cb97c304291300a6825d51c2e49f3412f4d564
robert-min <robertmin522@gmail.com>
2022-03-09 20:53:25 +0900

    pymysql로 test -> kafka connector로 진행해야할 듯

--- blog_db.py
+++ blog_db.py
@@ -1,9 +1,28 @@
 from kafka import KafkaConsumer
+import pymysql
 import json
 
 brokers = ["localhost:9091", "localhost:9092", "localhost:9093"]
 consumer = KafkaConsumer("blog_db", bootstrap_servers=brokers)
 
+db = pymysql.connect(host="localhost", port=3306, user="root", password="aksen5466!", db="todayDating", charset="utf8")
+
 for message in consumer:
     msg = json.loads(message.value.decode())
-    print(msg)
+    print(msg["keyword"])
+    try:
+        cursor = db.cursor()
+        sql = "INSERT IGNORE INTO blog (keyword, title, link, placeId) VALUES ('{0}', '{1}', '{2}', {3});".format(
+            msg["keyword"],
+            msg["title"],
+            msg["link"],
+            int(msg["placeId"])
+        )
+        print(sql)
+        cursor.execute(sql)
+
+        db.commit()
+    finally:
+        db.close()
+
+
