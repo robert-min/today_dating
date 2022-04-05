d3a07c72deeb95dc50a4137c34f0628060c7344f
robert-min <robertmin522@gmail.com>
2022-04-05 20:19:28 +0900

    find data code test

---
+++ flask_server/search.py
@@ -0,0 +1,87 @@
+import pymysql
+
+class rds:
+    def __init__(self, keyword):
+        self.keyword = keyword
+
+    def find_data(self):
+        db = pymysql.connect(host="database-1.c89wkz4nejak.ap-northeast-2.rds.amazonaws.com",
+                             port=3306,
+                             user="admin",
+                             password="Aksen5466!",
+                             db="todayDating",
+                             charset="utf8")
+
+        cursor = db.cursor()
+
+        sql = "SELECT * FROM blog WHERE keyword = '{}' ORDER BY RAND() LIMIT 5;".format(self.keyword)
+
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        all_card = []
+        for data in result:
+            context = data[2]
+            link = data[3]
+            place_id = data[4]
+
+            sql2 = "SELECT * FROM place WHERE placeId = '{}';".format(place_id)
+            cursor.execute(sql2)
+            temp = cursor.fetchone()
+            store_name = temp[1]
+            all_card.append([store_name, context, link])
+        output = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "listCard": {
+                            "header": {
+                                "title": "{} 데이트 장소 입니다.".format(temp)
+                            },
+                            "items": [
+                                {
+                                    "title": all_card[0][0],
+                                    "description": all_card[0][1],
+                                    "link": {
+                                        "web": all_card[0][2]
+                                    }
+                                },
+                                {
+                                    "title": all_card[1][0],
+                                    "description": all_card[1][1],
+                                    "link": {
+                                        "web": all_card[1][2]
+                                    }
+                                },
+                                {
+                                    "title": all_card[2][0],
+                                    "description": all_card[2][1],
+                                    "link": {
+                                        "web": all_card[2][2]
+                                    }
+                                },
+                                {
+                                    "title": all_card[3][0],
+                                    "description": all_card[3][1],
+                                    "link": {
+                                        "web": all_card[3][2]
+                                    }
+                                },
+                                {
+                                    "title": all_card[4][0],
+                                    "description": all_card[4][1],
+                                    "link": {
+                                        "web": all_card[4][2]
+                                    }
+                                },
+                            ]
+                        }
+                    },
+                    {"simpleText": {
+                        "text": "추가 검색을 원하실 경우\n키워드를 입력해주세요."}}
+                ]
+            }
+        }
+        db.close()
+        return output
+
--- flask_server/test.py
+++ flask_server/test.py
@@ -1,5 +1,12 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask, request, jsonify
 import sys
+import search
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 app = Flask(__name__)
 
 
@@ -10,65 +17,9 @@ def test():
         print(req)
         keyword = req["action"]["params"]["Subway"]
         print(keyword)
-        output = list_card(keyword)
+        output = search.rds(keyword)
     return jsonify(output)
 
-
-def list_card(temp):
-    output = {
-      "version": "2.0",
-      "template": {
-        "outputs": [
-          {
-            "listCard": {
-              "header": {
-                "title": "{} 데이트 장소 입니다.".format(temp)
-              },
-              "items": [
-                {
-                  "title": "챗봇 관리자센터",
-                  "description": "새로운 AI의 내일과 일상의 변화",
-                  "link": {
-                    "web": "https://namu.wiki/w/%EB%9D%BC%EC%9D%B4%EC%96%B8(%EC%B9%B4%EC%B9%B4%EC%98%A4%ED%94%84%EB%A0%8C%EC%A6%88)"
-                  }
-                },
-                {
-                  "title": "챗봇 관리자센터",
-                  "description": "카카오톡 채널 챗봇 만들기",
-                  "link": {
-                    "web": "https://namu.wiki/w/%EB%AC%B4%EC%A7%80(%EC%B9%B4%EC%B9%B4%EC%98%A4%ED%94%84%EB%A0%8C%EC%A6%88)"
-                  }
-                },
-                {
-                  "title": "Kakao i Voice Service",
-                  "description": "보이스봇 / KVS 제휴 신청하기",
-                  "link": {
-                    "web": "https://namu.wiki/w/%EC%96%B4%ED%94%BC%EC%B9%98"
-                  }
-                },
-                  {
-                  "title": "Kakao i Voice Service",
-                  "description": "보이스봇 / KVS 제휴 신청하기",
-                  "link": {
-                    "web": "https://namu.wiki/w/%EC%96%B4%ED%94%BC%EC%B9%98"
-                  }
-                },
-                  {
-                  "title": "Kakao i Voice Service",
-                  "description": "보이스봇 / KVS 제휴 신청하기",
-                  "link": {
-                    "web": "https://namu.wiki/w/%EC%96%B4%ED%94%BC%EC%B9%98"
-                  }
-                }
-              ]
-            }
-          },
-            {"simpleText": {
-               "text": "추가 검색을 원하실 경우\n키워드를 입력해주세요."}}
-        ]
-      }
-    }
-    return output
 
 
 if __name__ == "__main__":
