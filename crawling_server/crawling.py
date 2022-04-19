af65fc53752f2de3c14e501247f4322448b08c05
robert-min <robertmin522@gmail.com>
2022-04-19 19:42:40 +0900

    json 저장 코드 완료

--- crawling_server/crawling.py
+++ crawling_server/crawling.py
@@ -8,6 +8,7 @@ import math
 from bs4 import BeautifulSoup
 import time
 import secret
+from collections import defaultdict
 
 
 # 네이버 API
@@ -15,6 +16,7 @@ client_id = secret.naver_id
 client_secret = secret.naver_secret
 
 def get_blog_count(query, display):
+
     # OO 데이트로 기본 검색
     encode_query = urllib.parse.quote(query + '데이트')
     search_url = "https://openapi.naver.com/v1/search/blog?query=" + encode_query
@@ -45,6 +47,7 @@ def get_blog_count(query, display):
             print("Blog total : " + str(blog_total))
             print("Blog count : " + str(blog_count))
 
+
     return blog_count
 
 
@@ -61,12 +64,15 @@ def get_blog_post(query, display, start_index):
     response = urllib.request.urlopen(request)
     response_code = response.getcode()
 
-    all_contents = list()
+    all_contents = defaultdict(list)
+    all_contents["keyword"] = query
 
     if (response_code == 200):
         response_body = response.read()
         response_body_dict = json.loads(response_body.decode("utf-8"))
         contents = response_body_dict["items"]
+        all_contents["blog"] = contents
+        all_place = list()
 
         for item_index in range(0, len(contents)):
             link = contents[item_index]["link"]
@@ -83,40 +89,64 @@ def get_blog_post(query, display, start_index):
                     map_soup = blog_post_soup.find("div", attrs={"class" : "se-module se-module-map-text"})
                     place_text = map_soup.find("a")["data-linkdata"]
                     place_dict = json.loads(place_text)
-                    single_contents = {"blog": contents, "place": place_dict}
-                    all_contents.append(single_contents)
-                    print(item_index, "page 수집중")
+                    all_place.append(place_dict)
+
                 except:
                     pass
-
             item_index += 1
             time.sleep(0.1)
 
+        all_contents["place"] = all_place
+
     return all_contents
 
-def save_parquet(query, display, start_index):
-    data = get_blog_post(query, display, start_index)
+def save_json(query, display, start_index, name):
+    all_contents = get_blog_post(query, display, start_index)
 
-    from pyspark.sql import SparkSession
+    import json
+    import os
+    from time import time, localtime, strftime
 
-    MAX_MEMORY = "5g"
-    spark = SparkSession.builder.appName("today_dating") \
-        .config("spark.executor.memory", MAX_MEMORY) \
-        .config("spark.driver.memory", MAX_MEMORY) \
-        .getOrCreate()
+    tm = localtime(time())
+    strftime("%Y-%m-%d", tm)
 
-    df = spark.createDataFrame(data, schema=None)
-    df.write.json("./")
+    path = "./date_data/" + strftime("%Y-%m-%d", tm)
 
-    print("수집완료")
+    try:
+        if not os.path.isdir(path):
+            os.mkdir(path)
+    except OSError as e:
+        if e.errno != e.errno.EEXIST:
+            print("Failed to create")
+            raise
+
+    with open(path + "/{}.json".format(name), "w") as f:
+        json.dump(all_contents, f, indent=4, ensure_ascii=False)
+
+# def save_parquet(query, display, start_index):
+#     data = get_blog_post(query, display, start_index)
+#
+#     from pyspark.sql import SparkSession
+#
+#     MAX_MEMORY = "5g"
+#     spark = SparkSession.builder.appName("today_dating") \
+#         .config("spark.executor.memory", MAX_MEMORY) \
+#         .config("spark.driver.memory", MAX_MEMORY) \
+#         .getOrCreate()
+#
+#     df = spark.createDataFrame(data, schema=None)
+#     df.write.json("./")
+#
+#     print("수집완료")
 
 if __name__ == '__main__':
-    query = ["용답"]
+    query = ["용답", "신답", "용두", "신설동"]
     display = 100
     start = 1
-    for q in query:
+    for i, q in enumerate(query):
         blog_count = get_blog_count(q, display)
+        print(q, ":", blog_count)
         for start_index in range(start, blog_count + 1, display):
             print(start_index)
-            save_parquet(q, display, start_index)
+            save_json(q, display, start_index, i)
             print()
