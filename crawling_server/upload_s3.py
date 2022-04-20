c2cbe0b17c2c363b772b8e037a0fd7d97e264a86
robert-min <robertmin522@gmail.com>
2022-04-20 15:53:11 +0900

    s3 업로드 자동화 코드 구현

---
+++ crawling_server/upload_s3.py
@@ -0,0 +1,36 @@
+import boto3
+import secret
+import json
+import os
+from time import time, localtime, strftime
+
+
+def upload_file_s3(bucket_name, path, date):
+    # aws
+    ACCESS_KEY = secret.aws_access_id
+    SECRET_KEY = secret.aws_access_secret
+
+    s3 = boto3.client("s3", aws_access_key_id = ACCESS_KEY, aws_secret_access_key = SECRET_KEY)
+
+    file_list = os.listdir(path)
+    file_list_json = [file for file in file_list if file.endswith(".json")]
+
+    for file in file_list_json:
+        try:
+            with open(path + "/" + file, "rb") as data:
+                s3.put_object(Bucket=bucket_name,
+                              Body=data,
+                              Key=date + "/" + file)
+        except:
+            return print("error")
+
+
+
+if __name__ == '__main__':
+
+    tm = localtime(time())
+    date = strftime("%Y-%m-%d", tm)
+    path = "./date_data/" + date
+    bucket = "today-dating"
+    upload_file_s3(bucket, path, date)
+
